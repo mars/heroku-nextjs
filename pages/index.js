1775bf1e41d3318eccec33f95377ac634369d340
Mars Hall <mars@heroku.com>
2016-10-29 17:16:07 -0700

    Add demo content with basic styles.

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,22 @@
 import React from 'react'
-export default () => <marquee>Hello Next from Heroku!</marquee>
+import css from 'next/css'
+
+export default () => (
+  <div className={layoutStyle}>
+    <h1>Next.js on Heroku</h1>
+
+    <p>Deploy <a href="https://zeit.co/blog/next">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
+
+    <p>This <strong>demo deployment on Heroku</strong> is from the repo <a href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
+
+    <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
+  </div>
+)
+
+const layoutStyle = css({
+  fontFamily: 'sans-serif',
+  lineHeight: '1.33rem',
+  '@media (min-width: 600px)': {
+    margin: '0 20vw'
+  }
+})
