902eaf10026225869ee69a37750d4e919959d3f0
Mars Hall <mars@heroku.com>
2016-10-29 17:27:45 -0700

    💄

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Head from 'next/head'
 import css from 'next/css'
 
 export default () => (
   <div className={layoutStyle}>
+    <Head>
+      <meta charSet="utf-8"/>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <title>Next.js on Heroku</title>
+    </Head>
+
     <h1>Next.js on Heroku</h1>
 
     <p>Deploy <a href="https://zeit.co/blog/next">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
@@ -16,7 +24,9 @@ export default () => (
 const layoutStyle = css({
   fontFamily: 'sans-serif',
   lineHeight: '1.33rem',
+  marginTop: '8vh',
   '@media (min-width: 600px)': {
-    margin: '0 20vw'
+    marginLeft: '21vw',
+    marginRight: '21vw'
   }
 })
