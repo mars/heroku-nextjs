21451517e2db3f245fe1dcf86b2af584e5fdd04b
Mars Hall <mars@users.noreply.github.com>
2017-03-27 23:07:52 -0700

    Migrate to Next 2.0.0 beta (#11)

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
-import css from 'next/css'
 import Logo from '../components/logo'
 
 export default () => (
-  <div className={layoutStyle}>
+  <div className="root">
     <Head>
       <meta charSet="utf-8"/>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <title>Next.js on Heroku</title>
     </Head>
+    <style jsx>{`
+      .root {
+        font-family: sans-serif;
+        line-height: 1.33rem;
+        margin-top: 8vh,
+      }
+      @media (min-width: 600px) {
+        .root {
+          margin-left: 21vw;
+          margin-right: 21vw;
+        }
+      }
+    `}</style>
 
     <h1><Logo style={{ height: '1.45rem' }}/> Next.js on Heroku</h1>
 
@@ -21,13 +33,3 @@ export default () => (
     <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
   </div>
 )
-
-const layoutStyle = css({
-  fontFamily: 'sans-serif',
-  lineHeight: '1.33rem',
-  marginTop: '8vh',
-  '@media (min-width: 600px)': {
-    marginLeft: '21vw',
-    marginRight: '21vw'
-  }
-})
